b119ab0eecdfca9ee978a6d2c42fecd91b65020e
Sohee KWON <soheekwon@naver.com>
2021-10-22 12:31:35 +0900

    feat: [2차 세미나] 과제2 모달창 구현

--- velog_clone/script.js
+++ velog_clone/script.js
@@ -1,6 +1,28 @@
 const options = document.querySelectorAll(".option");
 const optionList = document.querySelector(".nav__options");
 const selectedOption = document.querySelector(".nav__select > p");
+const posts = document.querySelectorAll(".post");
+const modal = document.querySelector(".modal");
+
+const showPost = (index) => {
+  if (window.innerWidth > 767) {
+    modal.style.display = "block";
+    modal.appendChild(posts[index - 1]);
+    const modalPost = modal.querySelector(".post");
+    modalPost.style.margin = "auto";
+    modalPost.style.marginTop = "10%";
+    modalPost.onclick = null;
+
+    const cancelButton = document.createElement("button");
+    cancelButton.innerText = "X";
+    cancelButton.classList.add("modal__button");
+    cancelButton.addEventListener("click", () => {
+      modal.style.display = "none";
+      modal.innerHTML = "";
+    });
+    modal.appendChild(cancelButton);
+  }
+};
 
 const switchOption = (event) => {
   options.forEach((option) => option.classList.remove("selected"));
