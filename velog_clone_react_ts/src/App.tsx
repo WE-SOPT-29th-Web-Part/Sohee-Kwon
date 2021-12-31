c36715bca2db190b3c7ccf86711b90383540d398
Sohee KWON <soheekwon@naver.com>
2022-01-01 04:35:25 +0900

    feat: assets, libs, App.tsx, index.css 기본세팅

--- velog_clone_react_ts/src/App.tsx
+++ velog_clone_react_ts/src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Article from "./pages/Article";
+// import Home from "./pages/Home";
+// import Write from "./pages/Write";
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/write" element={<h1>Write</h1>} />
+          <Route path="/article/:id" element={<h1>Article</h1>} />
+          <Route path="/article/edit/:id" element={<h1>Write</h1>} />
+          <Route path="/*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
